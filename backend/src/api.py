0b67bfdd93cb251daa010471ac75a3fa76e3dfc9
root <root@vps543490.ovh.net>
2018-05-29 16:21:13 +0200

    cnx closed

--- backend/src/api.py
+++ backend/src/api.py
@@ -10,30 +10,43 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-cnx = db.connect()
 mod_password = open("mod_password", "r").readline().strip("\n")
 
 @app.route("/dictionary/<string:word>", methods=["GET"])
 def search_word(word):
+    cnx = db.connect()
     register.register_search(word, cnx)
     entry = signepedia.get_entry(word, cnx)
+    cnx.close()
     return jsonify(entry)
 
 @app.route("/counter", methods=["GET"])
 def count_videoIds():
-    return str(counter.videoIds(cnx))
+    cnx = db.connect()
+    number = str(counter.videoIds(cnx))
+    cnx.close()
+    return number
 
 @app.route("/new_entry/<string:newEntry>", methods=["GET"])
 def insert_new_entry(newEntry):
-    return unvalidated.insert(json.loads(newEntry), cnx)
+    cnx = db.connect()
+    answer = unvalidated.insert(json.loads(newEntry), cnx)
+    cnx.close()
+    return answer
 
 @app.route("/validate/<string:validEntry>", methods=["GET"])
 def validate_entry(validEntry):
     validEntry = json.loads(validEntry)
     if validEntry["password"] == mod_password:
-        return unvalidated.validate(validEntry, cnx)
+        cnx = db.connect()
+        answer = unvalidated.validate(validEntry, cnx)
+        cnx.close()
+        return answer
     return "wrong_password"
 
 @app.route("/get_unvalidated", methods=["GET"])
 def get_unvalidated():
-    return jsonify(unvalidated.get_unvalidated(cnx))
+    cnx = db.connect()
+    answer = jsonify(unvalidated.get_unvalidated(cnx))
+    cnx.close()
+    return answer
